6d7909cb8044ec0b64230722bdf9516e20cb5cd9
developer13-art <iliyaaabdurrazaq@gmail.com>
2024-09-20 22:40:37 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 document.getElementById('foam-btn').addEventListener('click', function() {
   window.location.href = 'Connect.html';
 });
+var typed = new Typed(".typing", {
+  strings: ["Web Developer, programmer, electrical engineer from Kaduna, Nigeria. <br>I create, design Websites for schools, <br>e-commerce for batter online activities."],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true
+});
+
 
 
